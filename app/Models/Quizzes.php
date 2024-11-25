5d8b1591294aa4948dbcc9551e1bf6a095011732
Khoiru Miftakhul Falah <mrcr4zysm1le@gmail.com>
2024-11-25 07:58:53 +0700

    fix (backend) : perbaikan model quiz

--- app/Models/Quizzes.php
+++ app/Models/Quizzes.php
@@ -7,13 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @property integer $id
  * @property integer $materi_id
- * @property string $judul
+ * @property integer $periode_id
+ * @property string $tanggal_mulai
+ * @property string $title
+ * @property integer $timer
  * @property string $deskripsi
  * @property string $created_at
  * @property string $updated_at
  * @property Question[] $questions
  * @property QuizAttempt[] $quizAttempts
  * @property Materi $materi
+ * @property Periode $periode
  * @property StudentQuizAttempt[] $studentQuizAttempts
  */
 class Quizzes extends Model
@@ -21,7 +25,7 @@ class Quizzes extends Model
     /**
      * @var array
      */
-    protected $fillable = ['materi_id', 'title', 'deskripsi', 'timer', 'created_at', 'updated_at'];
+    protected $fillable = ['materi_id', 'periode_id', 'tanggal_mulai', 'title', 'timer', 'deskripsi', 'created_at', 'updated_at'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -36,7 +40,7 @@ class Quizzes extends Model
      */
     public function quizAttempts()
     {
-        return $this->hasMany('App\Models\QuizAttempts', 'quizzes_id');
+        return $this->hasMany('App\Models\QuizAttempt', 'quizzes_id');
     }
 
     /**
@@ -45,6 +49,14 @@ class Quizzes extends Model
     public function materi()
     {
         return $this->belongsTo('App\Models\Materi');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function periode()
+    {
+        return $this->belongsTo('App\Models\Periode');
     }
 
     /**
